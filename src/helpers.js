8dcb154b14ffc4b0cd47b167bca9a7ba9c7b392c
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2018-02-16 01:04:11 +0100

    new build

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 const degreeToRadian = (degree) => degree * (3.14159 / 180);
 const zeroPointToCorner = (width, height, depth) => new THREE.Matrix4().makeTranslation(width / 2, height / 2, depth / 2);
-const filePath = (p) => process.env === 'production' ? p : 'dist/' + p;
+const filePath = (p) => process.env.NODE_ENV === 'production' ? p : 'dist/' + p;
 
 const helpers = {
 	degreeToRadian,
